95864dfe9d6ee1ca9d4d3445f9b37698a920abb0
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2019-05-16 21:38:02 +0530

    55 tests for english passing: Normal, PDF, CLEAN

--- tests/test_english.py
+++ tests/test_english.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
 import pytest
 import pySBD
-# from pySBD.segmenter import Segmenter
 
 
 TEST_CASES = [
@@ -110,17 +110,6 @@ TEST_CASES = [
         "a. The first item b. The second item c. The third list item",
         ["a. The first item", "b. The second item", "c. The third list item"],
     ),
-    # This is for doctype pdf
-    # ("This is a sentence\ncut off in the middle because pdf.",
-    #     ["This is a sentence\ncut off in the middle because pdf."]),
-    # requires clean=True
-    # ("It was a cold \nnight in the city.",
-    #     ["It was a cold night in the city."]),
-    # requires clean=True
-    # (
-    #     "features\ncontact manager\nevents, activities\n",
-    #     ["features", "contact manager", "events, activities"],
-    # ),
     (
         "You can find it at N°. 1026.253.553. That is where the treasure is.",
         [
@@ -172,13 +161,7 @@ TEST_CASES = [
             "One further habit which was somewhat weakened . . . was that of combining words into self-interpreting compounds.",
             ". . . The practice was not abandoned. . . ."
         ],
-    ),
-    # (
-    #     "Hello world.Today is Tuesday.Mr. Smith went to the store and bought 1,000.That is a lot.",
-    #     [
-    #         "Hello world.", "Today is Tuesday.",
-    #         "Mr. Smith went to the store and bought 1,000.", "That is a lot."
-    #     ])
+    )
 ]
 
 TESTS_WITH_CLEAN = [
@@ -190,6 +173,21 @@ TESTS_WITH_CLEAN = [
             ["Hello world.", "Today is Tuesday.",
                 "Mr. Smith went to the store and bought 1,000.",
                 "That is a lot."])
+        ]
+
+PDF_TEST_DATA = [
+    ("This is a sentence\ncut off in the middle because pdf.",
+        ["This is a sentence cut off in the middle because pdf."]),
+    ("Organising your care early \nmeans you'll have months to build a good relationship with your midwife or doctor, ready for \nthe birth.",
+        ["Organising your care early means you'll have months to build a good relationship with your midwife or doctor, ready for the birth."]),
+    ("10. Get some rest \n \nYou have the best chance of having a problem-free pregnancy and a healthy baby if you follow \na few simple guidelines:",
+        ["10. Get some rest", "You have the best chance of having a problem-free pregnancy and a healthy baby if you follow a few simple guidelines:"]),
+    # ("• 9. Stop smoking \n• 10. Get some rest \n \nYou have the best chance of having a problem-free pregnancy and a healthy baby if you follow \na few simple guidelines:  \n\n1. Organise your pregnancy care early",
+    #     ["• 9. Stop smoking", "• 10. Get some rest", "You have the best chance of having a problem-free pregnancy and a healthy baby if you follow a few simple guidelines:", "1. Organise your pregnancy care early"]),
+    ("Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next. First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards and book-shelves; here and there she saw maps and pictures hung upon pegs. She took down a jar from one of the shelves as she passed; it was labelled 'ORANGE MARMALADE', but to her great disappointment it was empty: she did not like to drop the jar for fear of killing somebody, so managed to put it into one of the cupboards as she fell past it.\n'Well!' thought Alice to herself, 'after such a fall as this, I shall think nothing of tumbling down stairs! How brave they'll all think me at home! Why, I wouldn't say anything about it, even if I fell off the top of the house!' (Which was very likely true.)",
+        ["Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next.", "First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards and book-shelves; here and there she saw maps and pictures hung upon pegs.", "She took down a jar from one of the shelves as she passed; it was labelled 'ORANGE MARMALADE', but to her great disappointment it was empty: she did not like to drop the jar for fear of killing somebody, so managed to put it into one of the cupboards as she fell past it.", "'Well!' thought Alice to herself, 'after such a fall as this, I shall think nothing of tumbling down stairs! How brave they'll all think me at home! Why, I wouldn't say anything about it, even if I fell off the top of the house!' (Which was very likely true.)"]),
+    ("Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next. First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards and book-shelves; here and there she saw maps and pictures hung upon pegs. She took down a jar from one of the shelves as she passed; it was labelled 'ORANGE MARMALADE', but to her great disappointment it was empty: she did not like to drop the jar for fear of killing somebody, so managed to put it into one of the cupboards as she fell past it.\r'Well!' thought Alice to herself, 'after such a fall as this, I shall think nothing of tumbling down stairs! How brave they'll all think me at home! Why, I wouldn't say anything about it, even if I fell off the top of the house!' (Which was very likely true.)",
+        ["Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next.", "First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards and book-shelves; here and there she saw maps and pictures hung upon pegs.", "She took down a jar from one of the shelves as she passed; it was labelled 'ORANGE MARMALADE', but to her great disappointment it was empty: she did not like to drop the jar for fear of killing somebody, so managed to put it into one of the cupboards as she fell past it.", "'Well!' thought Alice to herself, 'after such a fall as this, I shall think nothing of tumbling down stairs! How brave they'll all think me at home! Why, I wouldn't say anything about it, even if I fell off the top of the house!' (Which was very likely true.)"])
         ]
 
 
@@ -209,10 +207,8 @@ def test_en_sbd_clean(text, expected_sents):
     assert segments == expected_sents
 
 
-def test_en_pdf_type():
-    text, expected_sents = (
-        "This is a sentence\ncut off in the middle because pdf.",
-            ["This is a sentence cut off in the middle because pdf."])
+@pytest.mark.parametrize('text,expected_sents', PDF_TEST_DATA)
+def test_en_pdf_type(text, expected_sents):
     seg = pySBD.Segmenter(text, clean=True, doc_type='pdf')
     segments = seg.segment()
     assert segments == expected_sents
