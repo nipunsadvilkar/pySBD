80d1a67372860829b717723ea173e681a8c19e11
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2019-05-19 18:22:59 +0530

    Add abbreviation replacer, clean rules python files

---
+++ pySBD/abbreviation_replacer.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import re
+from pySBD.rules import Text
+# TODO: SENTENCE_STARTERS should be lang specific
+from pySBD.lang.standard import Abbreviation, SENTENCE_STARTERS
+from pySBD.lang.common.numbers import (Common, SingleLetterAbbreviationRules,
+                                       AmPmRules)
+
+
+def replace_multi_period_abbreviations(txt):
+    mpa = re.findall(Common.MULTI_PERIOD_ABBREVIATION_REGEX, txt, flags=re.IGNORECASE)
+    if not mpa:
+        return txt
+    for each in mpa:
+        replacement = re.sub(re.escape(r'.'), '∯', each)
+        text = re.sub(re.escape(each), replacement, txt)
+    return text
+
+
+def replace_pre_number_abbr(txt, abbr):
+    txt = re.sub(r'(?<=\s{abbr})\.(?=\s\d)|(?<=^{abbr})\.(?=\s\d)'.format(abbr=abbr.strip()), "∯", txt)
+    txt = re.sub(r'(?<=\s{abbr})\.(?=\s+\()|(?<=^{abbr})\.(?=\s+\()'.format(abbr=abbr.strip()), "∯", txt)
+    return txt
+
+
+def replace_prepositive_abbr(txt, abbr):
+    txt = re.sub(r'(?<=\s{abbr})\.(?=\s)|(?<=^{abbr})\.(?=\s)'.format(abbr=abbr.strip()), "∯", txt)
+    txt = re.sub(r'(?<=\s{abbr})\.(?=:\d+)|(?<=^{abbr})\.(?=:\d+)'.format(abbr=abbr.strip()), "∯", txt)
+    return txt
+
+
+def replace_period_of_abbr(txt, abbr):
+    txt = re.sub(r"(?<=\s{abbr})\.(?=((\.|\:|-|\?)|(\s([a-z]|I\s|I'm|I'll|\d|\())))|(?<=^{abbr})\.(?=((\.|\:|\?)|(\s([a-z]|I\s|I'm|I'll|\d))))".format(abbr=abbr.strip()), '∯', txt)
+    txt = re.sub(r"(?<=\s{abbr})\.(?=,)|(?<=^{abbr})\.(?=,)".format(abbr=abbr.strip()), '∯', txt)
+    return txt
+
+
+def replace_abbreviation_as_sentence_boundary(txt):
+    for word in SENTENCE_STARTERS:
+        escaped = re.escape(word)
+        regex = r"(U∯S|U\.S|U∯K|E∯U|E\.U|U∯S∯A|U\.S\.A|I|i.v)∯(?=\s{}\s)".format(escaped)
+        txt = re.sub(regex, '\\1.', txt)
+    return txt
+
+
+class AbbreviationReplacer(object):
+
+    def __init__(self, text, language='en'):
+        self.text = text
+        self.language = language
+
+    def replace(self):
+        self.text = Text(self.text).apply(Common.PossessiveAbbreviationRule,
+                                          Common.KommanditgesellschaftRule,
+                                          *SingleLetterAbbreviationRules.All)
+        self.text = self.search_for_abbreviations_in_string()
+        self.text = replace_multi_period_abbreviations(self.text)
+        self.text = Text(self.text).apply(*AmPmRules.All)
+        self.text = replace_abbreviation_as_sentence_boundary(self.text)
+        return self.text
+
+    def search_for_abbreviations_in_string(self):
+        original = self.text
+        lowered = original.lower()
+        for abbr in Abbreviation.ABBREVIATIONS:
+            stripped = abbr.strip()
+            if stripped not in lowered:
+                continue
+            abbrev_match = re.findall(
+                r'(?:^|\s|\r|\n){}'.format(stripped), original,
+                re.IGNORECASE)
+            if not abbrev_match:
+                continue
+            next_word_start = r"(?<={" + str(re.escape(stripped)) + "} ).{1}"
+            char_array = re.findall(next_word_start, self.text)
+            for ind, match in enumerate(abbrev_match):
+                self.text = self.scan_for_replacements(self.text, match, ind, char_array)
+        return self.text
+
+    def scan_for_replacements(self, txt, am, ind, char_array):
+        char = char_array[ind] if char_array else ''
+        prepositive = Abbreviation.PREPOSITIVE_ABBREVIATIONS
+        number_abbr = Abbreviation.NUMBER_ABBREVIATIONS
+        upper = str(char).isupper()
+        if (not upper or am.strip().lower() in prepositive):
+            if am.strip().lower() in prepositive:
+                txt = replace_prepositive_abbr(txt, am)
+            elif am.strip().lower() in number_abbr:
+                txt = replace_pre_number_abbr(txt, am)
+            else:
+                txt = replace_period_of_abbr(txt, am)
+        return txt
+
+
+if __name__ == "__main__":
+    s = "I have lived in the U.S. for 20 years."
+    print(AbbreviationReplacer(s).replace())
+    # I have lived in the U∮S∯ for 20 years.
---
+++ pySBD/clean/rules.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from pySBD.rules import Rule
+
+
+class CleanRules(object):
+
+    # NOTE: Caution: Might require \\ for special characters
+    # if regex is defined with r'' then dont
+    # add extra \\ for special characters
+    # Rubular: http://rubular.com/r/V57WnM9Zut
+    NewLineInMiddleOfWordRule = Rule(r'\n(?=[a-zA-Z]{1,2}\n)', '')
+
+    # Rubular: http://rubular.com/r/dMxp5MixFS
+    DoubleNewLineWithSpaceRule = Rule(r'\n \n', "\r")
+
+    # Rubular: http://rubular.com/r/H6HOJeA8bq
+    DoubleNewLineRule = Rule(r'\n\n', "\r")
+
+    # Rubular: http://rubular.com/r/FseyMiiYFT
+    NewLineFollowedByPeriodRule = Rule(r'\n(?=\.(\s|\n))', '')
+
+    ReplaceNewlineWithCarriageReturnRule = Rule(r'\n', "\r")
+
+    EscapedNewLineRule = Rule(r'\\n', "\n")
+
+    EscapedCarriageReturnRule = Rule(r'\\r', "\r")
+
+    TypoEscapedNewLineRule = Rule(r'\\\ n', "\n")
+
+    TypoEscapedCarriageReturnRule = Rule(r'\\\ r', "\r")
+
+    # Rubular: http://rubular.com/r/bAJrhyLNeZ
+    InlineFormattingRule = Rule(r'{b\^&gt;\d*&lt;b\^}|{b\^>\d*<b\^}', '')
+
+    # Rubular: http://rubular.com/r/8mc1ArOIGy
+    TableOfContentsRule = Rule(r'\.{4,}\s*\d+-*\d*', "\r")
+
+    # Rubular: http://rubular.com/r/DwNSuZrNtk
+    ConsecutivePeriodsRule = Rule(r'\.{5,}', ' ')
+
+    # Rubular: http://rubular.com/r/IQ4TPfsbd8
+    ConsecutiveForwardSlashRule = Rule(r'\/{3}', '')
+
+    # Rubular: http://rubular.com/r/6dt98uI76u
+    NO_SPACE_BETWEEN_SENTENCES_REGEX = r'(?<=[a-z])\.(?=[A-Z])'
+    # NO_SPACE_BETWEEN_SENTENCES_REGEX = r'[a-z]\.[A-Z]'
+    NoSpaceBetweenSentencesRule = Rule(NO_SPACE_BETWEEN_SENTENCES_REGEX, '. ')
+
+    # Rubular: http://rubular.com/r/l6KN6rH5XE
+    NO_SPACE_BETWEEN_SENTENCES_DIGIT_REGEX = r'(?<=\d)\.(?=[A-Z])'
+    NoSpaceBetweenSentencesDigitRule = Rule(NO_SPACE_BETWEEN_SENTENCES_DIGIT_REGEX, '. ')
+
+    URL_EMAIL_KEYWORDS = ['@', 'http', '.com', 'net', 'www', '//']
+
+    # Rubular: http://rubular.com/r/3GiRiP2IbD
+    NEWLINE_IN_MIDDLE_OF_SENTENCE_REGEX = r'(?<=\s)\n(?=([a-z]|\())'
+
+    # Rubular: http://rubular.com/r/Gn18aAnLdZ
+    NewLineFollowedByBulletRule = Rule(r"\n(?=•')", "\r")
+
+    QuotationsFirstRule = Rule(r"''", '"')
+    QuotationsSecondRule = Rule(r'``', '"')
+
+
+class HTML(object):
+    # Rubular: http://rubular.com/r/9d0OVOEJWj
+    HTMLTagRule = Rule(r"<\/?\w+((\s+\w+(\s*=\s*(?:\".*?\"|'.*?'|[\^'\">\s]+))?)+\s*|\s*)\/?>", '')
+
+    # Rubular: http://rubular.com/r/XZVqMPJhea
+    EscapedHTMLTagRule = Rule(r'&lt;\/?[^gt;]*gt;', '')
+
+    All = [HTMLTagRule, EscapedHTMLTagRule]
+
+
+class PDF(object):
+    # Rubular: http://rubular.com/r/UZAVcwqck8
+    NewLineInMiddleOfSentenceRule = Rule(r'(?<=[^\n]\s)\n(?=\S)', '')
+
+    # Rubular: http://rubular.com/r/eaNwGavmdo
+    NewLineInMiddleOfSentenceNoSpacesRule = Rule(r"\n(?=[a-z])", ' ')
