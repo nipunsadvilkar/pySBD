498332a7317a5175f47dae49359db954dd6136f6
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2019-06-06 20:20:13 +0530

    150 english tests passing

--- tests/test_english_clean.py
+++ tests/test_english_clean.py
@@ -202,7 +202,47 @@ TESTS_WITH_CLEAN = [
         ("I wrote this in the 'nineties.  It has four sentences.  This is the third, isn't it?  And this is the last",
             ["I wrote this in the 'nineties.", "It has four sentences.", "This is the third, isn't it?", "And this is the last"]),
         ("I wrote this in the ’nineties.  It has four sentences.  This is the third, isn't it?  And this is the last",
-            ["I wrote this in the ’nineties.", "It has four sentences.", "This is the third, isn't it?", "And this is the last"])
+            ["I wrote this in the ’nineties.", "It has four sentences.", "This is the third, isn't it?", "And this is the last"]),
+
+        # set clean=False
+        ("He has Ph.D.-level training", ["He has Ph.D.-level training"]),
+        # set clean=False
+        # ("He has Ph.D. level training", ["He has Ph.D. level training"])
+
+        # set clean=False
+        ("I will be paid Rs. 16720/- in total for the time spent and the inconvenience caused to me, only after completion of all aspects of the study.",
+            ["I will be paid Rs. 16720/- in total for the time spent and the inconvenience caused to me, only after completion of all aspects of the study."]),
+
+        # set clean=False
+        ("If I decide to withdraw from the study for other reasons, I will be paid only up to the extent of my participation amount according to the approved procedure of Apotex BEC. If I complete all aspects in Period 1, I will be paid Rs. 3520 and if I complete all aspects in Period 1 and Period 2, I will be paid Rs. 7790 and if I complete all aspects in Period 1, Period 2 and Period 3, I will be paid Rs. 12060 at the end of the study.",
+            ["If I decide to withdraw from the study for other reasons, I will be paid only up to the extent of my participation amount according to the approved procedure of Apotex BEC.", "If I complete all aspects in Period 1, I will be paid Rs. 3520 and if I complete all aspects in Period 1 and Period 2, I will be paid Rs. 7790 and if I complete all aspects in Period 1, Period 2 and Period 3, I will be paid Rs. 12060 at the end of the study."]),
+
+        # set clean=False
+        ("After completion of each Period, I will be paid an advance amount of rs. 1000 and this amount will be deducted from my final study compensation.",
+            ["After completion of each Period, I will be paid an advance amount of rs. 1000 and this amount will be deducted from my final study compensation."]),
+
+        # set clean=False
+        ("Mix it, put it in the oven, and -- voila! -- you have cake.",
+            ["Mix it, put it in the oven, and -- voila! -- you have cake."]),
+
+        # set clean=False
+        ("Some can be -- if I may say so? -- a bit questionable.",
+            ["Some can be -- if I may say so? -- a bit questionable."]),
+
+        # set clean=False
+        ("What do you see? - Posted like silent sentinels all around the town, stand thousands upon thousands of mortal men fixed in ocean reveries.",
+            ["What do you see?", "- Posted like silent sentinels all around the town, stand thousands upon thousands of mortal men fixed in ocean reveries."]),
+
+        # set clean=False
+        ("In placebo-controlled studies of all uses of Tracleer, marked decreases in hemoglobin (>15% decrease from baseline resulting in values <11 g/ dL) were observed in 6% of Tracleer-treated patients and 3% of placebo-treated patients. Bosentan is highly bound (>98%) to plasma proteins, mainly albumin.",
+            ["In placebo-controlled studies of all uses of Tracleer, marked decreases in hemoglobin (>15% decrease from baseline resulting in values <11 g/ dL) were observed in 6% of Tracleer-treated patients and 3% of placebo-treated patients.", "Bosentan is highly bound (>98%) to plasma proteins, mainly albumin."]),
+
+        # set clean=False
+        ("The parties to this Agreement are PragmaticSegmenterExampleCompanyA Inc. (“Company A”), and PragmaticSegmenterExampleCompanyB Inc. (“Company B”).",
+            ["The parties to this Agreement are PragmaticSegmenterExampleCompanyA Inc. (“Company A”), and PragmaticSegmenterExampleCompanyB Inc. (“Company B”)."]),
+
+        ("Unlike the abbreviations i.e. and e.g., viz. is used to indicate a detailed description of something stated before.",
+            ["Unlike the abbreviations i.e. and e.g., viz. is used to indicate a detailed description of something stated before."])
         ]
 
 PDF_TEST_DATA = [
