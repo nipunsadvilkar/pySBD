a4e0e05e27442574d3903b34587a2395994933b6
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2019-06-08 23:16:33 +0530

    Add README.md, setup file

---
+++ README.md
@@ -0,0 +1,41 @@
+# pySBD: Python Sentence Boundary Disambiguation (SBD)
+
+[![License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat)](https://github.com/nipunsadvilkar/pySBD/blob/master/LICENSE)
+
+pySBD - python Sentence Boundary Disambiguation (SBD) - is a rule-based sentence boundary detection module that works out-of-the-box across many languages.
+
+This project is a direct port of ruby gem - [Pragmatic Segmenter](https://github.com/diasks2/pragmatic_segmenter) which provides rule-based sentence boundary detection.
+
+
+## Install
+
+**Python**
+```
+pip install pysbd
+```
+
+## Usage
+
+* Currently pySBD support only English language. Support for more languages will be released soon.
+
+```python
+import pySBD
+text = "Hello World. My name is Jonas."
+seg = pySBD.Segmenter(text, clean=False)
+print(seg.segment())
+# ['Hello World.', 'My name is Jonas.']
+```
+
+## Contributing
+
+If you find a text that is incorrectly segmented using pySBD, please submit an issue.
+
+1. Fork it ( https://github.com/nipunsadvilkar/pySBD/fork )
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+
+# Credit
+This project wouldn't be possible without the great work done by [Pragmatic Segmenter](https://github.com/diasks2/pragmatic_segmenter) team.
---
+++ setup.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# inspired from:
+# https://github.com/kennethreitz/setup.py/blob/master/setup.py
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
+import os
+import sys
+from shutil import rmtree
+from setuptools import find_packages, setup, Command
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+
+with io.open(os.path.join(root, "pySBD", "about.py"), encoding="utf8") as f:
+    about = {}
+    exec(f.read(), about)
+
+# Import the README and use it as the long-description.
+with io.open(os.path.join(root, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+# Package meta-data.
+NAME = about['__title__']
+DESCRIPTION = about['__version__']
+URL = 'https://github.com/me/myproject'
+EMAIL = 'me@example.com'
+AUTHOR = 'Awesome Soul'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = about['__version__']
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(root, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(
+    name='pySBD',
+    version=about['__version__'],
+    description=about['__summary__'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=about["__author__"],
+    author_email=about["__email__"],
+    url=about["__uri__"],
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requires=[],
+    include_package_data=True,
+    license=about["__license__"],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        "Topic :: Scientific/Engineering",
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
