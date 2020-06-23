8fc9e7128ab2ca3d007675691267d6b92a2f9613
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2020-06-23 15:29:26 +0530

    🎨 ⚡️  Limit stanza nlp pipeline to tokenize

--- benchmarks/stanza_benchmark.py
+++ benchmarks/stanza_benchmark.py
@@ -1,6 +1,6 @@
 import stanza
 # stanza.download('en')
-nlp = stanza.Pipeline('en')
+nlp = stanza.Pipeline(lang='en', processors='tokenize')
 
 from .english_golden_rules import GOLDEN_EN_RULES
 
@@ -20,10 +20,10 @@ if __name__ == "__main__":
     import timeit
     time_taken = timeit.timeit("stanza_english_benchmark(GOLDEN_EN_RULES)",
                   setup="from __main__ import stanza_english_benchmark, GOLDEN_EN_RULES",
-                  number=10)
+                  number=100)
     print(f'{stanza.__name__} - v{stanza.__version__}')
     print(f'GRS score: {percent_score:0.2f}%')
     print(f'Speed: {time_taken*1000:>10.2f} ms')
     # stanza - v1.0.1
     # GRS score: 72.92%
-    # Speed:  109839.63 ms
+    # Speed:  120803.37 ms
