fc61aef0fb638240a65ff2ef3b1e49132e83270f
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2020-09-11 14:51:41 +0530

    🔖  Bump up version to v0.3.2

--- pysbd/about.py
+++ pysbd/about.py
@@ -2,7 +2,7 @@
 # https://python-packaging-user-guide.readthedocs.org/en/latest/single_source_version/
 
 __title__ = "pysbd"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __summary__ = "pysbd (Python Sentence Boundary Disambiguation) is a rule-based sentence boundary detection that works out-of-the-box across many languages."
 __uri__ = "http://nipunsadvilkar.github.io/"
 __author__ = "Nipun Sadvilkar"
